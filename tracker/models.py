61200aa8396c4679fa52b62715eecb626f3373e4
Jasmine <jayjadesanders@gmail.com>
2026-03-31 13:16:33 -0400

    Created migrationfile, fixed typo in models

--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Category(models.model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=7, default='#000000')
 
