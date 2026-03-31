ff5cb1936e94eb881d85bc8b83c17e0965bb9d63
Jasmine <jayjadesanders@gmail.com>
2026-03-31 18:05:28 -0400

    Added views for each model

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        qs = Transaction.objects.all()
+        month = self.request.query_params.get('month')
+        type = self.request.query_params.get('type')
+        min_amount = self.request.query_params.get('min_amount')
+        max_amount = self.request.query_params.get('max_amount')
+
+        if month:
+            year, m = month.split('-')
+            qs = qs.filter(date__year=year, date__month=m)
+        
+        if type:
+            qs = qs.filter(type=type)
+        
+        if min_amount:
+            qs = qs.filter(amount__gte=min_amount)
+
+        if max_amount:
+            qs = qs.filter(amount__lte=max_amount)
+        
+        return qs
+    
+
+    class BudgetViewSet(viewsets.ModelViewSet):
+        serializer_class = BudgetSerializer
+        qs = Budget.objects.all()
+ 
+
+    
